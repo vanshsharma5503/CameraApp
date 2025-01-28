208984a9be93da0a74439aedcc5d1a48e745be26
vansh sharma <vanshsharma5503@gmail.com>
2025-01-28 13:33:31 +0530

    Initial Commit

---
+++ PhotoCamera/PhotoCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCameraApp.swift
+//  PhotoCamera
+//
+//  Created by Vansh Sharma on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoCameraTests/PhotoCameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCameraTests.swift
+//  PhotoCameraTests
+//
+//  Created by Vansh Sharma on 28/01/25.
+//
+
+import Testing
+@testable import PhotoCamera
+
+struct PhotoCameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
